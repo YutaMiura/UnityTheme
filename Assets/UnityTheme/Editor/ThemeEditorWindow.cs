c0a4e6ac4cc828a90cddbb1acb1c2195838d4dfc
YutaMiura <yuta.moguai.miura@gmail.com>
2023-08-10 11:52:15 +0900

    Redraw when add theme or add entry

--- Assets/UnityTheme/Editor/ThemeEditorWindow.cs
+++ Assets/UnityTheme/Editor/ThemeEditorWindow.cs
@@ -25,74 +25,14 @@ namespace UnityTheme.Editor
             Debug.Log("call CreateGUI");
             var uxml = uxmlAsset.Instantiate();
             rootVisualElement.Add(uxml);
-            _rows = rootVisualElement.Q<ScrollView>("rows");
-
-            var controls = rootVisualElement.Q<VisualElement>("controls");
-            var createNewThemeButton = rootVisualElement.Q<Button>("CreateTheme");
-            var createNewEntriesButton = rootVisualElement.Q<Button>("CreateEntries");
-            var createNewThemeButtonDescription = rootVisualElement.Q<Label>("CreateNewThemeDescription");
-            var createNewEntriesButtonDescription = rootVisualElement.Q<Label>("CreateNewEntriesDescription");
-            
-            var header = rootVisualElement.Q<ThemeItemListHeader>("header");
-            if (IsReady)
-            {
-                _rows.style.display = DisplayStyle.Flex;
-                header.style.display = DisplayStyle.Flex;
-                controls.style.display = DisplayStyle.Flex;
-                createNewThemeButton.style.display = DisplayStyle.None;
-                createNewThemeButtonDescription.style.display = DisplayStyle.None;
-                createNewEntriesButton.style.display = DisplayStyle.None;
-                createNewEntriesButtonDescription.style.display = DisplayStyle.None;
-                var headerList = new List<string>()
-                {
-                    "Key"
-                };
-                headerList.AddRange(Themes.Instance.All.Select(t => t.Name));
-                header.columns = string.Join(",", headerList);
-                header.OnClickThemeAdd = OnClickThemeAdd;
-                
-                foreach (var key in Entries.Instance.AllKeys)
-                {
-                    var entries = Entries.Instance.EntriesByKey(key);
-                    var row = new ThemeItemRow();
-                    row.SetEntries(entries.ToArray());
-                    _rows.Add(row);
-                }
-                
-            } 
-            if (!IsThemesReady)
-            {
-                _rows.style.display = DisplayStyle.None;
-                header.style.display = DisplayStyle.None;
-                controls.style.display = DisplayStyle.None;
-                createNewThemeButton.style.display = DisplayStyle.Flex;
-                createNewThemeButtonDescription.style.display = DisplayStyle.Flex;
-            } 
-            if (!IsEntriesReady)
-            {
-                _rows.style.display = DisplayStyle.None;
-                header.style.display = DisplayStyle.None;
-                controls.style.display = DisplayStyle.None;
-                createNewEntriesButton.style.display = DisplayStyle.Flex;
-                createNewEntriesButtonDescription.style.display = DisplayStyle.Flex;
-            }
-            
-            createNewThemeButton.clicked += OnClickCreateNewThemeFirst;
-            createNewEntriesButton.clicked += OnClickCreateEntriesFirst;
-
-            var addButton = rootVisualElement.Q<Button>("AddButton");
-
-            addButton.clicked += OnClickAdd;
-            
-            rootVisualElement.MarkDirtyRepaint();
+            DrawContents();
+            AddHandlers();
         }
 
         private void OnClickThemeAdd()
         {
             Themes.Instance.AddTheme($"Theme {Themes.Instance.Count + 1}");
-            
-            var header = rootVisualElement.Q<ThemeItemListHeader>("header");
-            header.MarkDirtyRepaint();
+            DrawContents();
         }
 
         private void OnClickCreateEntriesFirst()
@@ -130,6 +70,72 @@ namespace UnityTheme.Editor
             _rows.Add(row);
             
             _rows.MarkDirtyRepaint();
+        }
+
+        private void DrawContents()
+        {
+            _rows = rootVisualElement.Q<ScrollView>("rows");
+
+            var controls = rootVisualElement.Q<VisualElement>("controls");
+            var createNewThemeButton = rootVisualElement.Q<Button>("CreateTheme");
+            var createNewEntriesButton = rootVisualElement.Q<Button>("CreateEntries");
+            var createNewThemeButtonDescription = rootVisualElement.Q<Label>("CreateNewThemeDescription");
+            var createNewEntriesButtonDescription = rootVisualElement.Q<Label>("CreateNewEntriesDescription");
+            
+            var header = rootVisualElement.Q<ThemeItemListHeader>("header");
+            if (IsReady)
+            {
+                _rows.style.display = DisplayStyle.Flex;
+                header.style.display = DisplayStyle.Flex;
+                controls.style.display = DisplayStyle.Flex;
+                createNewThemeButton.style.display = DisplayStyle.None;
+                createNewThemeButtonDescription.style.display = DisplayStyle.None;
+                createNewEntriesButton.style.display = DisplayStyle.None;
+                createNewEntriesButtonDescription.style.display = DisplayStyle.None;
+                var headerList = new List<string>()
+                {
+                    "Key"
+                };
+                headerList.AddRange(Themes.Instance.All.Select(t => t.Name));
+                header.columns = string.Join(",", headerList);
+                header.OnClickThemeAdd = OnClickThemeAdd;
+                
+                _rows.Clear();
+                foreach (var key in Entries.Instance.AllKeys)
+                {
+                    var entries = Entries.Instance.EntriesByKey(key);
+                    var row = new ThemeItemRow();
+                    row.SetEntries(entries.ToArray());
+                    _rows.Add(row);
+                }
+                
+            } 
+            if (!IsThemesReady)
+            {
+                _rows.style.display = DisplayStyle.None;
+                header.style.display = DisplayStyle.None;
+                controls.style.display = DisplayStyle.None;
+                createNewThemeButton.style.display = DisplayStyle.Flex;
+                createNewThemeButtonDescription.style.display = DisplayStyle.Flex;
+            } 
+            if (!IsEntriesReady)
+            {
+                _rows.style.display = DisplayStyle.None;
+                header.style.display = DisplayStyle.None;
+                controls.style.display = DisplayStyle.None;
+                createNewEntriesButton.style.display = DisplayStyle.Flex;
+                createNewEntriesButtonDescription.style.display = DisplayStyle.Flex;
+            }
+        }
+
+        void AddHandlers()
+        {
+            var createNewThemeButton = rootVisualElement.Q<Button>("CreateTheme");
+            var createNewEntriesButton = rootVisualElement.Q<Button>("CreateEntries");
+            createNewThemeButton.clicked += OnClickCreateNewThemeFirst;
+            createNewEntriesButton.clicked += OnClickCreateEntriesFirst;
+            var addButton = rootVisualElement.Q<Button>("AddButton");
+            addButton.clicked += OnClickAdd;
         }
 
         private void OnGUI()
